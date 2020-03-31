c19dbfa477244391016c2e2e3aa004f59c053f86
Amelie Certin <amelie.certin@gmail.com>
2020-03-31 16:23:54 +0200

    Run battle

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -6,16 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./battle.component.scss']
 })
 export class BattleComponent implements OnInit {
-  pokemon1 = {
-    name: 'Eevee'
-  }
-  pokemon2 = {
-    name: 'Pikachu'
-  }
-
+  pokemons = [
+    {name: 'Eevee', level: 5, hp: 20, hpMax: 20, attack: 20, defense: 20, speed: 20, moves: [{name: 'Nuzzle', power: 20}]},
+    {name: 'Pikachu', level: 5, hp: 20, hpMax: 20, attack: 20, defense: 20, speed: 20, moves: [{name: 'Thunder Shock', power: 40}]}
+  ]
   constructor() { }
 
   ngOnInit(): void {
+    this.run()
   }
 
+  run() {
+    let [attacker, defenser] = this.attackOrder();
+    do {
+      const move = this.pokemons[attacker].moves[0];
+      this.runTurn(attacker, defenser, move);
+      [attacker, defenser] = [defenser, attacker];
+    } while (this.pokemons[0].hp > 0 && this.pokemons[1].hp > 0);
+    console.log(`Winner: ${this.pokemons[defenser].name}`);
+    return this.pokemons[defenser];
+  }
+
+  runTurn(attacker, defenser, move) {
+    const damage = this.damage(this.pokemons[attacker], this.pokemons[attacker].moves[0], this.pokemons[defenser].defense);
+    this.pokemons[defenser].hp -= damage;
+    console.log(`${this.pokemons[attacker].name} deals ${damage} to ${this.pokemons[defenser].name}`);
+  }
+
+  attackOrder() {
+    return this.pokemons[0].speed > this.pokemons[1].speed ? [0, 1] : [1, 0];
+  }
+
+  damage(pokemon, move, opponentDefense) {
+    return Math.floor(Math.floor(Math.floor(2 * pokemon.level / 5 + 2) * pokemon.attack * move.power / opponentDefense) / 50) + 2;
+  }
 }
--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Move, MovePool } from '../../logic/move';
-import { Some } from '../../monad/maybe';
 
 @Component({
   selector: 'app-pokemon',
@@ -9,23 +7,13 @@ import { Some } from '../../monad/maybe';
 })
 
 export class PokemonComponent implements OnInit {
-  id : number = 1;
   @Input() name : string = '';
   level : number = 1;
   hp : number = 20;
   maxHp : number = 20;
-  attack : number = 20;
-  defense : number = 20;
-  speed : number = 20;
-  moves : MovePool;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.moves = new MovePool(
-      new Move("Nuzzle", 20, 0, 20, 100),
-      new Some(new Move("Thunder Shock", 40, 0, 30, 100))
-    )
   }
-
 }
