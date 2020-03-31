2aefa0bd1e7abb773f4794a5db505704a761dc96
Amelie Certin <amelie.certin@gmail.com>
2020-03-31 12:39:27 +0200

    Fix @Input

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class PokemonComponent implements OnInit {
   id = 1;
-  @Input('name') name = "";
+  @Input() name = '';
   level = 1;
   hp = 20;
   maxHp = 20;
