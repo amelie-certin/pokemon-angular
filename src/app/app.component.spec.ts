693cea7e76a8c0fba3aa79f86303c90afcdc1c7f
Amelie Certin <amelie.certin@gmail.com>
2020-03-31 10:37:23 +0200

    Rename title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'Project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Project');
+    expect(app.title).toEqual('Pokemon');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Project app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Pokemon app is running!');
   });
 });
