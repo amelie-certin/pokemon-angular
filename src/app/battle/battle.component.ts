6f8097dc9e58d55dd3c4d04de825361b2225787b
Amelie Certin <amelie.certin@gmail.com>
2020-04-01 12:26:53 +0200

    Use fighters from App component

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -9,11 +9,6 @@ import {Pokemon} from '../../logic/pokemon';
 export class BattleComponent implements OnInit {
 
   @Input() fighters: Pokemon[];
-
-  pokemons = [
-    {name: 'Eevee', level: 5, hp: 20, maxHp: 20, attack: 20, defense: 20, speed: 20, moves: [{name: 'Nuzzle', power: 20}]},
-    {name: 'Pikachu', level: 5, hp: 20, maxHp: 20, attack: 20, defense: 20, speed: 20, moves: [{name: 'Thunder Shock', power: 40}]}
-  ];
   running = false;
   action = 'Play';
   logs: string[] = [];
@@ -41,25 +36,33 @@ export class BattleComponent implements OnInit {
   async run() {
     let [attacker, defenser] = this.attackOrder();
     do {
-      const move = this.pokemons[attacker].moves[0];
+      const move = this.fighters[attacker].moves.moves[0];
+      console.log(move);
+
       await this.runTurn(attacker, defenser, move);
       [attacker, defenser] = [defenser, attacker];
-    } while (this.pokemons[0].hp > 0 && this.pokemons[1].hp > 0);
-    this.winnerLog = `Winner: ${this.pokemons[defenser].name}`;
+
+    } while (this.fighters[0].hp > 0 && this.fighters[1].hp > 0);
+    this.winnerLog = `Winner: ${this.fighters[defenser].name}`;
   }
 
   async runTurn(attacker, defenser, move) {
-    const damage = this.damage(this.pokemons[attacker], this.pokemons[attacker].moves[0], this.pokemons[defenser].defense);
-    this.pokemons[defenser].hp -= damage;
-    this.logs.push(`${this.pokemons[attacker].name} deals ${damage} to ${this.pokemons[defenser].name}`);
+    const damage = this.damage(this.fighters[attacker], this.fighters[defenser], move);
+    this.fighters[defenser].hp -= damage;
+    this.logs.push(`${this.fighters[attacker].name} deals ${damage} to ${this.fighters[defenser].name}`);
     await this.delay();
   }
 
   attackOrder() {
-    return this.pokemons[0].speed > this.pokemons[1].speed ? [0, 1] : [1, 0];
+    return this.fighters[0].speed > this.fighters[1].speed ? [0, 1] : [1, 0];
   }
 
-  damage(pokemon, move, opponentDefense) {
-    return Math.floor(Math.floor(Math.floor(2 * pokemon.level / 5 + 2) * pokemon.attack * move.power / opponentDefense) / 50) + 2;
+  damage(pokemon, opponent, move) {
+    const level = pokemon.level;
+    const attack = pokemon.attack().baseStat;
+    const power = move.power;
+    const defense = opponent.defense().baseStat;
+
+    return Math.floor(Math.floor(Math.floor(2 * level / 5 + 2) * attack * power / defense) / 50) + 2;
   }
 }
