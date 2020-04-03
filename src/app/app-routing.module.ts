a00bf3e6a53054790b064eb6745a1b5e01f99175
Amelie Certin <amelie.certin@gmail.com>
2020-04-03 12:32:50 +0200

    Use a router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BattleComponent } from './battle/battle.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'battle', component: BattleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
